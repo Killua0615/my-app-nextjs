6ef5c6cc17bea0625b3a2eedb09501eb136c7f0a
natsumi <natsu.bsk810@gmail.com>
2024-07-16 01:12:41 +0900

    Image displayed on front

--- pages/characters.js
+++ pages/characters.js
@@ -1,27 +1,53 @@
 // pages/characters.js
 
 import React from 'react';
+import fetch from 'isomorphic-unfetch';
+import { Container, Grid, Card, CardMedia, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 500,
+    margin: 'auto',
+    marginBottom: '20px'
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+});
 
 const Characters = ({ characters }) => {
-  // charactersを使ってUIを描画
+  const classes = useStyles();
+
   return (
-    <div>
-      {characters.map(character => (
-        <div key={character.id}>
-          <h2>{character.name}</h2>
-          {/* 他のcharacter情報を表示 */}
-        </div>
-      ))}
-    </div>
+    <Container maxWidth="md">
+      <Typography variant="h5" align="center" color="textPrimary" gutterBottom>
+        HUNTER×HUNTER character lists
+      </Typography>
+      <Grid container spacing={3}>
+        {characters.map((character) => (
+          <Grid item xs={12} sm={6} key={character.id}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                image={character.image_url}
+                title={character.name}
+              />
+              <Typography variant="h6" align="center" color="textPrimary" gutterBottom>
+                {character.name}
+              </Typography>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
 Characters.getInitialProps = async () => {
-  const res = await fetch('http://localhost:3000/api/characters');
-  console.log('Response:', res);
+  const res = await fetch('http://localhost:3000/characters');
   const characters = await res.json();
-  console.log('Characters:', characters);
-  console.log('Is characters an array?', Array.isArray(characters)); // 追加
   return { characters };
 };
 
